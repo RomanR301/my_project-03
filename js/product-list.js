ca48fb33f5a0b490f12b060844080d9751b0c553
Roman Savchuk <roman.sav15@gmail.com>
2019-05-04 16:11:36 +0300

    dynamical product section and modal window

--- js/product-list.js
+++ js/product-list.js
@@ -16,8 +16,7 @@ class ProductList {
     let productListDomString = ''
     products.forEach(product => {
       productListDomString +=
-       `<div class="product-item" data-toggle="modal"
-       data-target="#productInfoModal data-id="${product.id}">
+       `<div class="product-item">
           <div class="product-item__img-container">
             <div class="product-item__image">
               <img src="img/${product.image}" alt="${product.title}" />
@@ -25,14 +24,19 @@ class ProductList {
           </div>
           <div class="product-item__description-container">
             <div class="product-item__description">
-              <p>${product.title}</p>
+              
             </div>
-            <div class="product-item__price">
-              <span class="product-price">$${product.price}</span>
+            <div class="card-body">
+            <h5 class="card-title">${product.title}</h5>
+              <button class="btn btn-info" data-toggle="modal"
+                 data-target="#productInfoModal" data-id="${product.id}">Info
+              </button>
+            
+              <button class="btn btn-primary buy" data-id="${product.id}">
+                 $${product.price} - Buy
+              </button>
             </div>
-            <div class="product-order__container">
-              <input type="image" src="img/buy.png">
-            </div>
+           
           </div>
         </div>`
       
@@ -41,24 +45,24 @@ class ProductList {
   }
   addEventListeners() {
     $('#productInfoModal').on('show.bs.modal', event => {
-      const button = $(event.relatedTarget); 
-      const id = String(button.data('id'));
-      const product = this.getProductById(id);
-      const modal = $ ('#productInfoModal');
-      modal.find('.modal-body .card-img-top')
-        .attr('src', 'img'+product.image)
-        .attr('alt', product.title);
-      // modal.find('.modal-body .card-title').text(product.title);
-      modal.find('.modal-body .card-text').text(product.description);
-      modal.find('button.buy')
-        // .text(`${product.price} - Buy`)
-        .data('id', id);
+        const button = $(event.relatedTarget); // Button that triggered the modal
+        const id  = String(button.data('id')); // Extract info from data-* attributes
+        const product = this.getProductById(id);
+        const modal = $('#productInfoModal');
+        modal.find('.modal-body .card-img-top')
+            .attr('src', 'img'+product.image)
+            .attr('alt', product.title);
+        modal.find('.modal-body .card-title').text(product.title);
+        modal.find('.modal-body .card-text').text(product.description);
+        modal.find('button.buy')
+            .text(`${product.price} - Buy`)
+            .data('id', id);
     });
     $('.card.product button.buy, #productInfoModal button.buy').click( event => {
-      const button = $(event.target);
-      const id = button.data('id');
-      this.cart.addProduct(id);
-      window.showAlert('Product added to card');
+        const button = $(event.target);
+        const id  = button.data('id'); 
+        this.cart.addProduct(id);
+        window.showAlert('Product added to cart');
     });
   }
- }
+}
